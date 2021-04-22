a8237750651caec98166a6633ad560d7e3347721
DarkRacer <schtscherbakov@gmail.com>
2021-04-23 01:30:28 +0300

    change server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const session = require('express-session')
 const app = express();
 
 app.use(express.static('./dist/frontend'));
+app.set('trust proxy', 1)
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  proxy: true,
+  cookie: {
+    secure: true,
+    maxAge: 360
+  }
+}))
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/frontend/'}),

6b50e546d1eff4ebe56297450f4bb45655b13a9b
DarkRacer <schtscherbakov@gmail.com>
2021-04-28 02:52:22 +0300

    Changed proxy

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const API_SERVER = process.env.API_SERVER;
 
 app.use(express.static('./dist/frontend'));
 app.set('trust proxy', 1);
@@ -9,6 +12,20 @@ app.use(cors({
   credentials: true,
   origin: "https://search-gift-backend.herokuapp.com/"
 }))
+
+app.use('/api', createProxyMiddleware({
+  logLevel     : 'debug',
+  target       : API_SERVER,
+  changeOrigin : true,
+  secure       : false,
+  xfwd         : true,
+  onProxyReq   : function (proxyReq, req, res) {
+    if (proxyReq.getHeader('origin')) {
+      proxyReq.setHeader('origin', API_SERVER);
+    }
+  }
+}));
+
 app.use(session({
   secret: 'secret',
   credentials: true,

459ef8532dfa449b8774171d0a8501891b78351b
DarkRacer <schtscherbakov@gmail.com>
2021-05-20 21:00:17 +0300

    Added paginator for self selection

--- frontend/src/app/components/self-dialog/self-dialog.component.ts
+++ frontend/src/app/components/self-dialog/self-dialog.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { SelectionService } from '../../services/selection.service';
 import { AssortmentModel } from '../../model/assortment.model';
@@ -7,6 +7,9 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { CategoryModel } from '../../model/category.model';
 import { FormControl } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { merge, startWith, switchMap, tap } from 'rxjs/operators';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-self-dialog',
   templateUrl: './self-dialog.component.html',
@@ -18,13 +21,16 @@ export class SelfDialogComponent implements OnInit {
   columns = ['name', 'price', 'function'];
   selection = new SelectionModel<ProductModel>(true, []);
   products: ProductModel[] = [];
+  dataSource = new MatTableDataSource<ProductModel>();
   resultsLength = 0;
-  pageSize = 5;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  // @ts-ignore
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(
     private readonly selectionService: SelectionService,
-    @Inject(MAT_DIALOG_DATA) public data: ProductModel[]
+    @Inject(MAT_DIALOG_DATA) public data: ProductModel[],
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -40,7 +46,10 @@ export class SelfDialogComponent implements OnInit {
   findProduct() {
     this.selectionService.findProduct(this.categoryCtrl.value).subscribe(e => {
       this.products = e;
+      this.dataSource.data = this.products;
+      this.cdr.detectChanges();
       this.resultsLength = this.products.length;
+      this.dataSource.paginator = this.paginator;
     })
   }
 }

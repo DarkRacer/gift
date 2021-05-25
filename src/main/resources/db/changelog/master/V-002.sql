0529f13aaf174b0fe9bb0817a22de5942e3cb919
DarkRacer <schtscherbakov@gmail.com>
2021-05-25 21:40:00 +0300

    Added database migration for roles

---
+++ src/main/resources/db/changelog/master/V-002.sql
@@ -0,0 +1,2 @@
+INSERT INTO gift.role (name) VALUES ('ROLE_USER');
+INSERT INTO gift.role (name) VALUES ('ROLE_ADMIN');
---
+++ src/main/resources/db/changelog/rollback/V-002.rollback.sql
@@ -0,0 +1,2 @@
+DELETE FROM gift.role WHERE role_id = 1;
+DELETE FROM gift.role WHERE role_id = 2;
